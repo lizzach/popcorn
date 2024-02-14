d70a350bb23cc94920b9a74a8637cef376000f8a
Elizabeth Zachary <elizabethzachary96@gmail.com>
2024-02-14 17:52:33 -0500

    Add color, size, and className props to StarRating and Star components

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -11,21 +11,31 @@ const starContainerStyle = {
   display: "flex",
 }
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0"
-}
-
 interface StarRatingProps {
   maxRating: number;
+  color: string;
+  size: number;
+  className: string;
 }
 
-export default function StarRating({maxRating = 5}: StarRatingProps) {
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = ""
+  }: StarRatingProps) {
   const [rating, setRating] = useState<number>(0);
   const [tempRating, setTempRating] = useState<number>(0);
 
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`
+  }
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({length: maxRating}, (_, i) => 
           <Star 
@@ -34,6 +44,8 @@ export default function StarRating({maxRating = 5}: StarRatingProps) {
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
             />)}
       </div>
       <p style={textStyle}>{tempRating || rating || ""}</p>
