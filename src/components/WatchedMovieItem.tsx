4fa30ff374e12a6feea6d9e8bd00978b4c213a18
Elizabeth Zachary <elizabethzachary96@gmail.com>
2024-02-14 14:02:57 -0500

    Add WatchedMovieItem component to render in WatchedMoviesList

---
+++ src/components/WatchedMovieItem.tsx
@@ -0,0 +1,28 @@
+import WatchedMovie from "../models/watchedMovie";
+
+interface WatchedMovieItemProps {
+  movie: WatchedMovie;
+}
+
+export default function WatchedMovieItem({movie}: WatchedMovieItemProps) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>
+  );
+}
