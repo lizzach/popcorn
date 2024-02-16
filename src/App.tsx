f7459fbe93d99d35c3f49ea9a16971aa6a28f10e
Elizabeth Zachary <elizabethzachary96@gmail.com>
2024-02-16 12:07:59 -0500

    Add node types and implement useEffect to load movie data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main.js";
 import "./index.css";
@@ -13,11 +13,23 @@ import MoviesList from "./components/MoviesList.js";
 import Summary from "./components/Summary.js";
 import WatchedMoviesList from "./components/WatchedMoviesList.js";
 
+const KEY = import.meta.env.VITE_API_KEY;
+
 function App() {
-  const [movies, setMovies] = useState<Movie[]>(tempMovieData);
+  const [movies, setMovies] = useState<Movie[]>([]);
   const [query, setQuery] = useState<string>("");
   const [isOpen, setIsOpen] = useState<boolean>(true);
   const [watched, setWatched] = useState<WatchedMovie[]>(tempWatchedData);
+  const queryTest = "Dune";
+
+  useEffect(function() {
+    async function fetchMovies() {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${queryTest}`);
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
+  }, [])
 
   return (
     <>
